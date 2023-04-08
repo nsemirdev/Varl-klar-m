085263f057282058410416960ee7b0b0c7552354
nsemirdev <nsemirdev@gmail.com>
2023-04-08 19:26:37 +0300

    Initial Commit

---
+++ Varlıklarım/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Varlıklarım
+//
+//  Created by Emir Alkal on 8.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
